110553e7e1a3c4158ee65a3e3ece608af3c08664
BbKem <kemRT142407@gmail.com>
2026-04-10 00:23:17 +0300

    fix: onreza deploy

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.use(express.static("dist"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("dist", "index.html"));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
